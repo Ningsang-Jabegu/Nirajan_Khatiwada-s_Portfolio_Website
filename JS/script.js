52875c3384537d0eb2a8d7dff165416e0e89ecc4
Ningsang Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2023-02-27 09:43:46 +0545

    js in progress

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,11 @@
-const dropdown = document.querySelector(".dropdown");
-const dropdown_content = document.querySelector(".dropdown-content");
-
-dropdown.addEventListener("click", function () {
-  alert("hi there");
-  dropdown_content.style.display = "block";
-});
-
-document.addEventListener("click", function (event) {
-if (!dropdown.contains(event.target)) {
-  dropdown_content.style.display = "none";
+function NK_header() {
+  const create_header = document.createElement("header");
+  const create_div_header = document.createElement("div");
+  create_div_header.classList.add("name_logo");
+  const create_img_header = document.createElement("img");
+  create_img_header.setAttribute("src", "/images/main_logo_name.png");
+  create_img_header.classList.add("name_logo");
+  create_div_header.appendChild(create_img_header);
+  create_header.appendChild(create_div_header);
+  document.body.appendChild(create_header);
 }
-});
