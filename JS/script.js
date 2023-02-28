ffca673dd17446b8fa0a751f02ccfb2bf946de6e
Ningsang Jabegu <86409265+Ningsang-Jabegu@users.noreply.github.com>
2023-02-28 10:14:38 +0545

    src parameter for header made

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,10 @@
 /* Function that creates logo header */
-function NK_header() {
+function NK_header(src="/images/main_logo_name.png") {
   const create_header = document.createElement("header");
   const create_div_header = document.createElement("div");
   create_div_header.classList.add("name_logo");
   const create_img_header = document.createElement("img");
-  create_img_header.setAttribute("src", "/images/main_logo_name.png");
+  create_img_header.setAttribute("src", src);
   create_img_header.classList.add("name_logo");
   create_div_header.appendChild(create_img_header);
   create_header.appendChild(create_div_header);
